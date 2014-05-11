d50bd203ebab1a83c598c6404984474eb70bd7c9
Porous Morris <professorq@gmail.com>
2014-05-11 09:24:31 -0700

    Fixed the logic - pretty sure it's better now

--- euler_3/factor/factor.go
+++ euler_3/factor/factor.go
@@ -6,7 +6,7 @@ import "math"
 func GreatestPrime(product int)  int {
     for i := 2; int(math.Pow(float64(i), 2)) < product; i++ {
         if product % i == 0 {
-           return product / i
+           product = product / i
         }
     }
     return product
