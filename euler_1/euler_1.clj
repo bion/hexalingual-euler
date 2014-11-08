a0765f290c016e291df49432d408437fe3af93ec
bion <bionjohnson@gmail.com>
2014-11-08 00:57:54 -0800

    Clojure solution for problem 1

---
+++ euler_1/euler_1.clj
@@ -0,0 +1,36 @@
+(defn three-or-five-mult [n]
+  (if (some #(= 0 %) [(mod n 5) (mod n 3)])
+    n
+    nil
+    ))
+
+(def sum-three-fives-multiples-below
+  (fn [n]
+    (loop [current 1 acc 0]
+      (if (= n current)
+        acc
+        (recur
+         (inc current)
+         (+ acc (or (three-or-five-mult current) 0)))))))
+
+(defn sum-three-fives-multiples-below2 [n]
+  (let [
+        threes (range 0 n 3)
+        fives (range 0 n 5)
+        distinct-multiples (distinct (concat threes fives))
+        ]
+    (reduce + distinct-multiples)
+    ))
+
+(use 'clojure.set)
+(defn sum-three-fives-multiples-below3 [n]
+  (let [
+        threes (range 0 n 3)
+        fives (range 0 n 5)
+        distinct-multiples (set (union threes fives))
+        ]
+    (reduce + distinct-multiples)))
+
+(str "The answer is: " (sum-three-fives-multiples-below 1000))
+(str "The answer is: " (sum-three-fives-multiples-below2 1000))
+(str "The answer is: " (sum-three-fives-multiples-below3 1000))
