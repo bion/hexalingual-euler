415f2f8cce37ed12226e62666527d768b96a79dc
bion <bionjohnson@gmail.com>
2014-11-17 09:03:37 -0800

    Update 4

--- euler_4/euler_4.clj
+++ euler_4/euler_4.clj
@@ -1,12 +1,13 @@
 (defn palindrome? [x]
   (= (reverse (str x)) (seq (str x))))
 
-(defn find-highest-palindrome [input-seq]
-  (reduce (fn [acc product]
-            (if (and (palindrome? product) (> product acc))
-              product acc))
-          input-seq))
-
 (def digits (range 100 1000))
 
-(time (println "The answer is:" (find-highest-palindrome (seq (for [a digits b digits] (* a b))))))
+(time (println "The answer is:"
+               (apply max
+                (for [a digits
+                      b digits
+                      :while (>= a b)
+                      :let [c (* a b)]
+                      :when (palindrome? c)]
+                  c))))
